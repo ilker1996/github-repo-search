9bd3be62972c3ff3e11d4ea95b9740428a147875
ilker <19080242+ilker1996@users.noreply.github.com>
2022-02-19 22:48:47 +0300

    Use axios for now

---
+++ src/service/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const fetchRepositories = (repoName, page, itemPerPage) => {
+  const endpoint = `https://api.github.com/search/repositories`;
+  
+  const response = axios.get(`${endpoint}?q=${repoName} in:name&page=${page}&per_page=${itemPerPage}`);
+  
+  return response;
+}
